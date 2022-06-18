9269d08a7d7cabd92dc64fa8456da68f3b0d075c
volnei <volnei.filho@best2bee.com.br>
2022-06-18 12:03:00 -0300

    Implementando dinâmica de de tela de listagem de partidas e giro 360 de dados

--- app/src/main/java/me/dio/simulator/ui/MainActivity.kt
+++ app/src/main/java/me/dio/simulator/ui/MainActivity.kt
@@ -1,17 +1,18 @@
 package me.dio.simulator.ui
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import me.dio.simulator.data.MatchesApi
 import me.dio.simulator.databinding.ActivityMainBinding
 import me.dio.simulator.domain.Match
 import me.dio.simulator.ui.adapter.MatchesAdapter
 import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -43,30 +44,40 @@ class MainActivity : AppCompatActivity() {
     private fun setupMatchesList() {
         binding.rvMatches.setHasFixedSize(true)
         binding.rvMatches.layoutManager = LinearLayoutManager(this)
+        findMatchesFromApi()
+    }
 
+    private fun setupMatchesRefresh() =
+        binding.srlMatches.setOnRefreshListener(this::findMatchesFromApi)
+
+
+    private fun setupFloatingActionButton() {
+        binding.fabSimulate.setOnClickListener { view ->
+            view.animate().rotationBy(360F).setDuration(500)
+                .setListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator?) {
+                    }
+                })
+        }
+    }
+
+    private fun findMatchesFromApi() {
+        binding.srlMatches.isRefreshing = true
         matchesApi.getMatches().enqueue(object : Callback<List<Match>> {
-            override fun onFailure(call: Call<List<Match>>, t: Throwable) {
-                Log.i("SIMULATOR", "Deu tudo errado: ${t.message}")
-            }
-
             override fun onResponse(call: Call<List<Match>>, response: Response<List<Match>>) {
                 if (response.isSuccessful) {
                     val matches: List<Match>? = response.body();
                     val matchesAdapter = matches?.let { MatchesAdapter(it) }
                     binding.rvMatches.adapter = matchesAdapter
-//                    Log.i("SIMULATOR", "Deu tudo certo! Partidas: " + matches?.size)
+                    //                    Log.i("SIMULATOR", "Deu tudo certo! Partidas: " + matches?.size)
                 } else Log.i("SIMULATOR", "Deu tudo errado: ${response.message()}")
+                binding.srlMatches.isRefreshing = false
             }
 
+            override fun onFailure(call: Call<List<Match>>, t: Throwable) {
+                binding.srlMatches.isRefreshing = false
+                Log.i("SIMULATOR", "Deu tudo errado: ${t.message}")
+            }
         })
-    }
-
-
-    private fun setupMatchesRefresh() {
-        // TODO Atualizar as partidas na ação de swipe
-    }
-
-    private fun setupFloatingActionButton() {
-        // TODO Criar evento de click e simulação de partidas.
     }
 }
