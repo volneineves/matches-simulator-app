27d6d938847529d77f00d75c5c7ec2f0cb9e4d48
volnei <volnei.filho@best2bee.com.br>
2022-06-18 11:52:50 -0300

    Glide implemented

--- app/src/main/java/me/dio/simulator/ui/adapter/MatchesAdapter.kt
+++ app/src/main/java/me/dio/simulator/ui/adapter/MatchesAdapter.kt
@@ -3,6 +3,7 @@ package me.dio.simulator.ui.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import me.dio.simulator.databinding.MatchItemBinding
 import me.dio.simulator.domain.Match
 
@@ -19,10 +20,13 @@ class MatchesAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val context = holder.itemView.context
         val match = matches[position]
 
         // Adapta os dados da partida (recuperada da API) para o nosso layout.
+        Glide.with(context).load(match.homeTeam.image).circleCrop().into(holder.binding.ivHomeTeam)
         holder.binding.tvHomeTeamName.text = match.homeTeam.name
+        Glide.with(context).load(match.awayTeam.image).circleCrop().into(holder.binding.ivAwayTeam)
         holder.binding.tvAwayTeamName.text = match.awayTeam.name
     }
 
